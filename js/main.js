1df4549361a905ddb191a8cd50b5c795eb192c8d
Clocon <luiste49@gmail.com>
2022-09-22 13:08:17 +0200

    codigo del inicio de anterior repositorio añadido a este

---
+++ js/main.js
@@ -0,0 +1,40 @@
+async function GET(url){
+  try{ 
+  const request =await fetch(url)
+  const data = await request.json()
+  document.querySelector("#titulo").innerHTML=data.header.title
+  document.querySelector("#titulo-des").innerHTML=data.header.description
+  document.querySelector("#historia").innerHTML= data.history.title
+  document.querySelector("#historia-des").innerHTML=data.history.description
+  document.querySelector("#location").innerHTML=data.location.title
+  document.querySelector("#location-des").innerHTML=data.location.description
+  document.querySelector("#food").innerHTML=data.meals.title
+  document.querySelector("#food-des").innerHTML=data.meals.description
+  document.querySelector("#gym").innerHTML=data.fitness.title
+  document.querySelector("#gym-des").innerHTML=data.fitness.description
+  document.querySelector("#pie").innerHTML=data.footer.text
+  }
+  catch{
+    console.warn("No ha sido posible descargar la información")
+  } 
+} 
+GET("https://practica-eoi.alexdw.com/contents")
+document.querySelector("#check").innerHTML="Checking ONLINE"
+
+const saludo =()=>{
+  const today=new Date()
+  const hora=today.getHours()
+
+  if(hora>=04 && hora<12){
+    document.querySelector("#saludo").innerHTML="Buenos días"
+    document.getElementById("saludo").style.color =" #9ab755";
+  }
+  if(hora>=12 && hora< 20){
+    document.querySelector("#saludo").innerHTML="Buenos tardes"
+    document.getElementById("saludo").style.color ="#7e3333";
+  }
+  if(hora>=20 || hora<04 ){
+    document.querySelector("#saludo").innerHTML="Buenos noches"
+    document.getElementById("saludo").style.color ="#000"
+}}
+saludo()
